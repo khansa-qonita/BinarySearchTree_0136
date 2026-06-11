6ece1732e8f55a73b873793d3e2a23d66a4bef13
khansa-qonita <khansaqonita420@gmail.com>
2026-06-11 09:33:59 +0700

    inisialisasi ROOT ke null

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -10,10 +10,10 @@ public :
     Node *rightchild;
 
     // constructor for the node class
-    Node (string i, Node *1, Node *r)
+    Node (string i, Node *l, Node *r)
     {
         info = i;
-        leftchild = 1;
+        leftchild = l;
         rightchild = r;
     }
 };
@@ -21,5 +21,10 @@ public :
 class BinaryTree
 {
 public :
-    
+    Node *ROOT;
+
+    BinaryTree()
+    {
+        ROOT = NULL;   //initializing ROOT to null
+    }
 }
