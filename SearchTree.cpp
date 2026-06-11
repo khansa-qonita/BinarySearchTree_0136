c9cc14314a712a84d456ca4cdca27d2240683a16
khansa-qonita <khansaqonita420@gmail.com>
2026-06-11 11:09:15 +0700

    membuat fungsi search untuk mencari currentNode dan parent

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -62,7 +62,19 @@ public :
     //as well as the current node of its parent
     void search (string element, Node *&parent, Node *&currentNode)
     {
-        
+        currentNode = ROOT;
+        parent = NULL;
+
+        while ((currentNode != NULL) &&
+        (currentNode->info != element))
+        {
+            parent = currentNode;
+
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
     }
 
 }
