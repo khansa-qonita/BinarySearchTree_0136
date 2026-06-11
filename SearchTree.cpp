cef84f229937af8ca324a98588ff81bf3ab47ca5
khansa-qonita <khansaqonita420@gmail.com>
2026-06-11 11:24:34 +0700

    implementasi fungsi postorder pada binary search tree

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -109,4 +109,21 @@ public :
         }
     }
 
-}
+    void postorder (Node *ptr)
+    {
+        //performs the postorder traversal of the tree
+
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }   
+    }
+};
